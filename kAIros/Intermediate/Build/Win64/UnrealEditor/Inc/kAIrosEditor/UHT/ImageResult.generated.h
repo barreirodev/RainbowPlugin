67edcba8c12998c7957a245324576460d337dfa0
Cristina <cristinabarreirodev@gmail.com>
2024-09-12 13:41:46 +0200

    Revert "Delete unnecesary folders"
    This reverts commit 4ecca8ff0159f3e79992bc6975abb988482a7bd9.

---
+++ kAIros/Intermediate/Build/Win64/UnrealEditor/Inc/kAIrosEditor/UHT/ImageResult.generated.h
@@ -0,0 +1,64 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "ImageResult.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef KAIROSEDITOR_ImageResult_generated_h
+#error "ImageResult.generated.h already included, missing '#pragma once' in ImageResult.h"
+#endif
+#define KAIROSEDITOR_ImageResult_generated_h
+
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_SPARSE_DATA
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_ACCESSORS
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAImageResult(); \
+	friend struct Z_Construct_UClass_AImageResult_Statics; \
+public: \
+	DECLARE_CLASS(AImageResult, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/kAIrosEditor"), NO_API) \
+	DECLARE_SERIALIZER(AImageResult)
+
+
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AImageResult(AImageResult&&); \
+	NO_API AImageResult(const AImageResult&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AImageResult); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AImageResult); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AImageResult) \
+	NO_API virtual ~AImageResult();
+
+
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_9_PROLOG
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_SPARSE_DATA \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_ACCESSORS \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_INCLASS_NO_PURE_DECLS \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> KAIROSEDITOR_API UClass* StaticClass<class AImageResult>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_ImageResult_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ kAIros/Intermediate/Build/Win64/UnrealEditor/Inc/kAIrosEditor/UHT/Utils.generated.h
@@ -0,0 +1,66 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Utils.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef KAIROSEDITOR_Utils_generated_h
+#error "Utils.generated.h already included, missing '#pragma once' in Utils.h"
+#endif
+#define KAIROSEDITOR_Utils_generated_h
+
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_SPARSE_DATA
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_ACCESSORS
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUUtils(); \
+	friend struct Z_Construct_UClass_UUtils_Statics; \
+public: \
+	DECLARE_CLASS(UUtils, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/kAIrosEditor"), NO_API) \
+	DECLARE_SERIALIZER(UUtils)
+
+
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UUtils(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UUtils(UUtils&&); \
+	NO_API UUtils(const UUtils&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UUtils); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UUtils); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UUtils) \
+	NO_API virtual ~UUtils();
+
+
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_7_PROLOG
+#define FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_SPARSE_DATA \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_ACCESSORS \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_INCLASS_NO_PURE_DECLS \
+	FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h_10_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> KAIROSEDITOR_API UClass* StaticClass<class UUtils>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_HostProject_Plugins_kAIros_Source_kAIrosEditor_Public_Utils_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
